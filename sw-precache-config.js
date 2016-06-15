2a6ca25cab8ecdc6056e1b28c6a5c25d72fdc80c
Marcel Samyn <dev@mrcl.io>
2016-06-15 10:05:45 +0200

    More compact styling, better offline supportImprove styles & offline support

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,8 @@ module.exports = {
     staticFileGlobs: [
         '/index.html',
         '/manifest.json',
-        '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+        '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+        '/src/*.html'
     ],
     navigateFallback: '/index.html',
     navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/|\/__).*/]
